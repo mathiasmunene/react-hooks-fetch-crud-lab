d66ff7832e5a130e37dfbc8440fc718a31127d58
mathiasmunene <mathiasmunene@gmail.com>
2025-05-29 17:38:12 +0300

    final

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,17 @@
 module.exports = {
   presets: [
-    "@babel/preset-env",
+    [
+      "@babel/preset-env",
+      {
+        targets: {
+          node: "12" // Target Node.js 12.x for CodeGrade
+        }
+      }
+    ],
     ["@babel/preset-react", { runtime: "automatic" }],
   ],
+  plugins: [
+    "@babel/plugin-proposal-optional-chaining"
+  ]
 };
+
